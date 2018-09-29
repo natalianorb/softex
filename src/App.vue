a7c4ee087131988885f642ef62bdea0d7a46fc6a
Natalia Norboeva <natanor@softomate.com>
2018-09-29 22:48:51 +0700

    sticky toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
     <v-app>
-      <v-toolbar>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn flat><router-link to="/">Home</router-link> </v-btn>
-          <v-btn flat><router-link to="/about">About Us</router-link></v-btn>
-          <v-btn flat><router-link to="/contact">Contact</router-link></v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-      <router-view/>
+      <div class="sticky">
+        <div class="sticky__fixed">
+          <v-toolbar>
+            <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn flat><router-link to="/">Home</router-link> </v-btn>
+                <v-btn flat><router-link to="/about">About Us</router-link></v-btn>
+                <v-btn flat><router-link to="/contact">Contact</router-link></v-btn>
+              </v-toolbar-items>
+          </v-toolbar>
+        </div>  
+        <div class="sticky__placeholder">
+        </div>    
+      </div>
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
     </v-app>
 </template>
 
@@ -19,5 +29,13 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.sticky__fixed {
+  position: fixed;
+  left: 0; right: 0;
+  z-index: 1;
+}
+.sticky__placeholder {
+  padding-top: 64px;
 }
 </style>
